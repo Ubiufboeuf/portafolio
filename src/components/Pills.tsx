dc84ed9e433879aa8fd37252dc5ffe409317b0ca
Ubiufboeuf <fede.2007.artencio.u@gmail.com>
2026-04-15 18:00:58 -0300

    feat: agregar iconos y pildoras de tecnologías

---
+++ src/components/Pills.tsx
@@ -0,0 +1,59 @@
+import { Icon } from './Icon'
+import { IconAstro, IconTypeScript, IconPreact, IconBun, IconTailwind, IconExpress } from './Icons'
+
+const pillClasses = 'flex items-center justify-center gap-1.5 h-fit w-fit p-2 px-2.5 rounded-md text-xs'
+const iconClasses = 'h-4 w-auto'
+
+export const AstroPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconAstro />
+    </Icon>
+    Astro
+  </div>
+)
+
+export const TypeScriptPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconTypeScript />
+    </Icon>
+    TypeScript
+  </div>
+)
+
+export const PreactPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconPreact />
+    </Icon>
+    Preact
+  </div>
+)
+
+export const BunPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconBun />
+    </Icon>
+    Bun
+  </div>
+)
+
+export const TailwindPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconTailwind />
+    </Icon>
+    Tailwind CSS
+  </div>
+)
+
+export const ExpressPill = () => (
+  <div class={pillClasses}>
+    <Icon class={iconClasses}>
+      <IconExpress />
+    </Icon>
+    Express
+  </div>
+)
